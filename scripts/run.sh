ba027cc74efb30bf86d81f0cf1eeaee55888abaa
Neil MacDougall <nmacdougall@suse.com>
2024-11-26 21:59:34 +0000

    Set env vars

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,9 @@
 
 echo "Starting Backstage"
 
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+export NODE_OPTIONS=--no-node-snapshot
+
 node /app/update-config
 node packages/backend --config app-config-rancher.yaml
 
